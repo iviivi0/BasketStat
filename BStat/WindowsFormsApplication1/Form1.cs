5b42f099c06b96d0ac4ed1095a1ea4d1f1a852d7
iviivi0 <vika.inshakova.2008@gmail.com>
2025-02-06 19:40:08 +0300

    add project
    Добавили проект

---
+++ BStat/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click( object sender, EventArgs e )
+    {
+      int i;
+      Int32.TryParse( this.textBox1.Text, out i );
+      i = BStat.Class1.first( i );
+      this.label1.Text = i.ToString();
+
+    }
+  }
+}
